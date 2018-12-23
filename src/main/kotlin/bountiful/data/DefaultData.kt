22a3f218e1bc42212990dc628e5f1356e7708f67
Ejektaflex <dootydooter@gmail.com>
2018-12-23 16:49:13 -0600

    Added additional default vanilla drops

--- src/main/kotlin/bountiful/data/DefaultData.kt
+++ src/main/kotlin/bountiful/data/DefaultData.kt
@@ -13,7 +13,16 @@ object DefaultData {
             PickableEntry("minecraft:cactus", 2..32, 80),
             PickableEntry("minecraft:diamond", 1..8, 2000),
             PickableEntry("minecraft:dispenser", 1..6, 200),
-            PickableEntry("minecraft:iron_ingot", 1..32, 200)
+            PickableEntry("minecraft:iron_ingot", 1..32, 200),
+            PickableEntry("minecraft:bread", 1..12, 60),
+            PickableEntry("minecraft:rotten_flesh", 1..24, 35),
+            PickableEntry("minecraft:sign", 1..16, 65),
+            PickableEntry("minecraft:slime_ball", 1..32, 60),
+            PickableEntry("minecraft:spider_eye", 1..16, 45),
+            PickableEntry("minecraft:string", 1..24, 35),
+            PickableEntry("minecraft:sugar", 1..64, 30),
+            PickableEntry("minecraft:tripwire_hook", 1..8, 60),
+            PickableEntry("minecraft:wheat", 1..48, 20)
     )
 
     val rewards = listOf(
