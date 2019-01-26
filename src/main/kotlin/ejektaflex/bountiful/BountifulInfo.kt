7f6fd91f6db31d8d1844a2131f91d41ebac8e571
Ejektaflex <dootydooter@gmail.com>
2019-01-26 01:16:11 -0600

    Put version range on Forgelin dependency

--- src/main/kotlin/ejektaflex/bountiful/BountifulInfo.kt
+++ src/main/kotlin/ejektaflex/bountiful/BountifulInfo.kt
@@ -5,7 +5,7 @@ object BountifulInfo {
     const val MODID = "bountiful"
     const val NAME = "Bountiful"
     const val VERSION = "2.1.0"
-    const val DEPENDS = "required-after:forgelin;"
+    const val DEPENDS = "required-after:forgelin@[1.8.0,1.9.0);"
     const val CLIENT = "ejektaflex.bountiful.proxy.ClientProxy"
     const val SERVER = "ejektaflex.bountiful.proxy.CommonProxy"
 }
