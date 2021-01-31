e9f55e8396f145fd3104fa290f2d1df7aa7d0cbe
Ejektaflex <dootydooter@gmail.com>
2021-01-30 20:17:49 -0600

    Switched to build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,79 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	//id 'com.github.johnrengelman.shadow' version '6.1.0'
+	kotlin("jvm") version "1.4.21"
+	kotlin("plugin.serialization") version "1.4.21"
+	id("fabric-loom") version "0.5-SNAPSHOT"
+}
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_1_8
+	targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+val modId: String by project
+val modVersion: String by project
+val group: String by project
+val minecraftVersion: String by project
+val fabricVersion: String by project
+val kotlinVersion: String by project
+val loaderVersion: String by project
+val yarnMappings: String by project
+
+project.group = group
+version = modVersion
+
+
+
+//compileKotlin.kotlinOptions.jvmTarget = "1.8"
+
+repositories {
+	mavenCentral()
+	//jcenter()
+	maven(url = "https://kotlin.bintray.com/kotlinx")
+	maven(url = "http://maven.fabricmc.net/") {
+		name = "Fabric"
+	}
+}
+
+minecraft {
+
+}
+
+dependencies {
+	//to change the versions see the gradle.properties file
+	minecraft("com.mojang:minecraft:${minecraftVersion}")
+	mappings("net.fabricmc:yarn:${yarnMappings}:v2")
+	modImplementation("net.fabricmc:fabric-loader:${loaderVersion}")
+
+	modApi("org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.1")
+	include("org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.1")
+	modApi("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+	include("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+
+	compile("com.google.code.findbugs:jsr305:3.0.2")
+
+	// Fabric API. This is technically optional, but you probably want it anyway.
+	modImplementation("net.fabricmc.fabric-api:fabric-api:${fabricVersion}")
+}
+
+
+tasks.getByName<ProcessResources>("processResources") {
+	filesMatching("fabric.mod.json") {
+		expand(
+			mutableMapOf<String, String>(
+				"modid" to modId,
+				"version" to modVersion,
+				"kotlinVersion" to kotlinVersion,
+				"fabricApiVersion" to fabricVersion
+			)
+		)
+	}
+}
+
+tasks.withType<KotlinCompile>() {
+	kotlinOptions {
+		jvmTarget = "1.8"
+	}
+}
