a35e36d8ed90f7fa3ccfa078e4ed20b13d51f325
Alex Sarnese <40846040+TelepathicGrunt@users.noreply.github.com>
2021-03-18 00:20:38 -0400

    Added ability for customizing bounty's blocks per village type
    I never touched kotlin before but hopefully I did alright lol.
    
    The idea here is that in SetupLifecycle, you pass in one more resourcelocation for what processor to use based on village type. In JigsawHelper, it grabs the processor from the registry (and defaults to minecraft:empty by default if there is no processor which is what you were using before)
    
    All that is now needed is to just make processor files in data/bountiful/worldgen/processor_list folder and name the files on the type of village. That's it! I included a badlands processor which will replace the bounty board structure piece's blocks with dark oak instead. Check it out for how it works.
    
    Just fyi, you must list every block property in output_state and block_state. If you dont, the processor dies. More info can be found here: https://minecraft.gamepedia.com/Custom_world_generation#Processor_lists
    
    I hope this helps! This will let you customize the bounty boards for vanilla villages as well!

--- src/main/kotlin/ejektaflex/bountiful/worldgen/JigsawHelper.kt
+++ src/main/kotlin/ejektaflex/bountiful/worldgen/JigsawHelper.kt
@@ -6,16 +6,23 @@ import net.minecraft.util.registry.Registry
 import net.minecraft.world.gen.feature.jigsaw.JigsawPattern
 import net.minecraft.world.gen.feature.jigsaw.JigsawPattern.PlacementBehaviour
 import net.minecraft.world.gen.feature.jigsaw.JigsawPiece
+import net.minecraft.world.gen.feature.template.ProcessorLists
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper
 
 
 object JigsawHelper {
 
-    fun registerJigsaw(server: MinecraftServer, nbtLocation: ResourceLocation, poolLocation: ResourceLocation, weight: Int = 10_000) {
+    fun registerJigsaw(server: MinecraftServer, nbtLocation: ResourceLocation, poolLocation: ResourceLocation, processorLocation: ResourceLocation, weight: Int = 10_000) {
 
         val pool = server.func_244267_aX()
             .getRegistry(Registry.JIGSAW_POOL_KEY).entries
             .find { it.key.location.toString() == poolLocation.toString() }?.value
+
+
+        var processorLists = server.func_244267_aX()
+                .getRegistry(Registry.STRUCTURE_PROCESSOR_LIST_KEY)
+                .getOptional(processorLocation)
+                .orElse(ProcessorLists.field_244101_a) // minecraft:empty
 
         val pieceList = ObfuscationReflectionHelper
             .getPrivateValue<ArrayList<JigsawPiece>, JigsawPattern>(
@@ -24,7 +31,7 @@ object JigsawHelper {
                 "field_214953_e"
             ) as MutableList<JigsawPiece>
 
-        val piece = JigsawPiece.func_242849_a(nbtLocation.toString()).apply(PlacementBehaviour.RIGID)
+        val piece = JigsawPiece.func_242851_a(nbtLocation.toString(), processorLists).apply(PlacementBehaviour.RIGID)
 
         repeat(weight) {
             pieceList.add(piece)
